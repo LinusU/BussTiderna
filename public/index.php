742fdc6d49d86c0f646f92a5df817d44547a004f
Linus Unnebäck <linus@folkdatorn.se>
2011-07-24 16:07:53 +0200

    Smarty3 and cleanup

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 /* Requirements */
+require_once 'smarty3/Smarty.class.php';
 require_once '/opt/Buzzmix/require.php';
 
 /* Create the Buzzmix */
@@ -12,19 +13,6 @@ $site->plugins_dir[] = dirname(__FILE__) . '/../plugins/';
 /* Set header and footer */
 $site->header_tpl = 'header.tpl';
 $site->footer_tpl = 'footer.tpl';
-
-/* Setup the MySQL Database */
-//$site->mysql_setup("localhost", "orkafota", "orkafota", "orkafota");
-
-/* What is the user requesting? */
-$pos = strpos($_SERVER['REQUEST_URI'], basename(__FILE__));
-
-if($pos === false) {
-    $uri = $_SERVER['REQUEST_URI'];
-} else {
-    $uri = substr($_SERVER['REQUEST_URI'], $pos + strlen(basename(__FILE__)));
-}
-/**/
 
 /* Fix for jQueryUI */
 if(substr($uri, 0, 8) == "/images/") {
